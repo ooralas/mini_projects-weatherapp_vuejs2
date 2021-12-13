5de1c3d4275546840d642ecc45e70ae007c7523f
ooralas <63547913+ooralas@users.noreply.github.com>
2021-12-13 11:44:58 +0100

    update server

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const path = require('path')
 const app = express()
 
 app.use('/',serveStatic(path.join(__dirname,'/dist')))
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
 
 const Port = process.env.PORT || 8080
 
